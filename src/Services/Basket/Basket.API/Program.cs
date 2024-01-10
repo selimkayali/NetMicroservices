705eb04f7e581c052db9f13b34c2ccbf47bb015d
selimkayali <selimkayali@gmail.com>
2024-01-11 00:17:34 +0200

    added containerization support for discount.grpc

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -3,64 +3,62 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
-using Microsoft.Extensions.Caching.Distributed;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Diagnostics;
 
-namespace Basket.API
+namespace Basket.API;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public IConfiguration Configuration { get; }
-        public static void Main(string[] args)
+        var configBuilder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+        var config = configBuilder.Build();
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        InjectRedis(builder.Services, config);
+        InjectDependencies(builder.Services, config);
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
         {
-            var configBuilder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
-            var config = configBuilder.Build();
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            InjectRedis(builder.Services, config);
-            InjectDependencies(builder.Services, config);
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
 
-        private static void InjectDependencies(IServiceCollection services, IConfiguration configuration)
-        {
-            services.AddScoped<IBasketRepository, BasketRepository>();
-            services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(opt =>
-            {
-                opt.Address = new Uri(configuration.GetValue<string>("GrpcSettings:DiscountUrl"));
-            });
-            services.AddScoped<DiscountGrpcService>();
+        app.UseAuthorization();
 
-        }
 
-        private static void InjectRedis(IServiceCollection services, IConfiguration configuration)
+        app.MapControllers();
+
+        app.Run();
+    }
+
+    private static void InjectDependencies(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IBasketRepository, BasketRepository>();
+        Debug.WriteLine(configuration.GetValue<string>("GrpcSettings:DiscountUrl"));
+        services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(opt =>
         {
-            services.AddStackExchangeRedisCache(opt =>
-            {
-                opt.Configuration = configuration.GetValue<string>("CacheSettings:ConnectionString");
-            });
-        }
+            opt.Address = new Uri(configuration.GetValue<string>("GrpcSettings:DiscountUrl"));
+        });
+        services.AddScoped<DiscountGrpcService>();
+
+    }
+
+    private static void InjectRedis(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddStackExchangeRedisCache(opt =>
+        {
+            opt.Configuration = configuration.GetValue<string>("CacheSettings:ConnectionString");
+        });
     }
 }
--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,40 +1,47 @@
 using Discount.Grpc.Extensions;
 using Discount.Grpc.Repositories;
 using Discount.Grpc.Services;
-//using Discount.Grpc.Services;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
-namespace Discount.Grpc
+namespace Discount.Grpc;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddGrpc(opt =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            opt.EnableDetailedErrors = true;
+        });
 
-            // Add services to the container.
-            builder.Services.AddGrpc();
+        //builder.WebHost.ConfigureKestrel(opt =>
+        //{
+        //    opt.ListenAnyIP(8003, o => o.Protocols = HttpProtocols.Http2);
+        //});
 
-            InjectDependencies(builder.Services);
-
-
-            var app = builder.Build();
-
-            app.MigrateDatabase<Program>();
-
-            // Configure the HTTP request pipeline.
-            app.MapGrpcService<DiscountService>();
-
-            app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
-            app.Run();
+        InjectDependencies(builder.Services);
 
 
-        }
+        var app = builder.Build();
 
-        private static void InjectDependencies(IServiceCollection services)
-        {
-            services.AddScoped<IDiscountRepository, DiscountRepository>();
-            services.AddAutoMapper(typeof(Program));
-        }
+        app.MigrateDatabase<Program>();
+
+        // Configure the HTTP request pipeline.
+        app.MapGrpcService<DiscountService>();
+
+        app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+        app.Run();
+
+
+    }
+
+    private static void InjectDependencies(IServiceCollection services)
+    {
+        services.AddScoped<IDiscountRepository, DiscountRepository>();
+        services.AddAutoMapper(typeof(Program));
     }
 }
